44adab78bb6e15a77f085339a20e34a2ddb19e5b
avil13 <a.1.3@mail.ru>
2014-04-03 18:50:22 +0400

    1 step for touch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+jQuery(document).ready(function($) {
+    var CSSLoad = function(file) {
+        // $('link[rel=stylesheet]').remove(); // del CSS
+        var link = document.createElement("link");
+        link.setAttribute("rel", "stylesheet");
+        link.setAttribute("type", "text/css");
+        link.setAttribute("href", file);
+        document.getElementsByTagName("head")[0].appendChild(link);
+    };
 
+    var showRecomendation = function() {
+
+    };
+
+
+    $(document).on({
+        'touchmove': showRecomendation()
+    });
+});
