f2b83c88a8b12a0ea195ae0859b753dc78019e1a
avil13 <13.avil@gmail.com>
2014-04-03 23:53:01 +0400

    optimazing page for touch device

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($) {
+
     var CSSLoad = function(file) {
         // $('link[rel=stylesheet]').remove(); // del CSS
         var link = document.createElement("link");
@@ -10,10 +11,18 @@ jQuery(document).ready(function($) {
 
     var showRecomendation = function() {
 
+        CSSLoad('/css/touch.css');
+
+        $('.social-text').click(function() {
+            $(this).hide();
+            $('.social-btns').find('a').show();
+            return false;
+        });
+
     };
 
+    if (Modernizr.touch) {
+        showRecomendation();
+    }
 
-    $(document).on({
-        'touchmove': showRecomendation()
-    });
 });
