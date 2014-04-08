d4dadc22d861e177e4b9b165c3981c83bc73e169
avil13 <13.avil@gmail.com>
2014-04-09 01:41:37 +0400

    social links

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+var htmlspecialchars = function (string) {
+    string = string.replace(/\//g, '%2F').replace(/\s/g, '+').replace(/:/g, '%3A');
+    return string;
+};
+
+
+
 jQuery(document).ready(function($) {
 
     var CSSLoad = function(file) {
@@ -27,10 +34,18 @@ jQuery(document).ready(function($) {
 
     // http://highlightjs.org/
     hljs.initHighlightingOnLoad();
-    hljs.configure({tabReplace: '    '});
+    hljs.configure({
+        tabReplace: '    '
+    });
     $('.doc').each(function(i, e) {
         hljs.highlightBlock(e);
     });
+
+
+    $('.a-vk').attr('href', 'http://vkontakte.ru/share.php?url=' + htmlspecialchars(window.location.href));
+    $('.a-fb').attr('href', 'http://www.facebook.com/sharer.php?u=' + htmlspecialchars(window.location.href));
+    $('.a-tw').attr('href', 'http://twitter.com/home?status=' + htmlspecialchars(window.location.href +"\n"+ $('title').text()));
+
 
 
 });
