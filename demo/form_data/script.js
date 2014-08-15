4ff97261a9e4aa325827448681a581feefd68a89
avil13 <a.1.3@mail.ru>
2014-08-15 18:40:47 +0400

    Update form data demo

--- demo/form_data/script.js
+++ demo/form_data/script.js
@@ -1,10 +1,13 @@
 jQuery(document).ready(function($) {
 
     $('#form').submit(function() {
-        var data = $('#form').form_data(),
-            txt = JSON.stringify(data, undefined, 4);
-        $('#cnsle').html(txt);
 
+        var data = $('#form').formData({
+            callback: function(data) {
+                var txt = JSON.stringify(data, undefined, 4);
+                $('#cnsle').html(txt);
+            }
+        });
         return false;
     });
 
