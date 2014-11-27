9957eea876dca6fe8f6a127985edc3953f9582f7
avil13 <13.avil@gmail.com>
2014-11-27 14:09:30 +0300

    Add comment

--- demo/anchorSpy/script.js
+++ demo/anchorSpy/script.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 
     var Spy = $('.menu').anchorSpy({
-            margin: 50,
+            margin: 100,
             speed: 1000,
             next: '.next',
             prev: '.prev',
