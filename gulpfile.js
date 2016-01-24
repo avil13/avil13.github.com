611b3121e58c917cbea88c28488bb574fdbc0bc3
avil13 <13.avil@gmail.com>
2016-01-24 16:35:25 +0300

    list repo

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,24 @@
 
 var gulp = require('gulp');
 var rename = require('gulp-rename');
-var concatCSS = require('gulp-concat-css');
-var minifyCSS = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var uncss = require('gulp-uncss');
 var importCss = require('gulp-import-css');
-
 var concat = require('gulp-concat');
-var jsmin = require('gulp-jsmin');
-var js1k = require("gulp-js1k");
+var uglify = require('gulp-uglify');
 
 // работа с css
 gulp.task('css', function() {
     gulp.src(['css/normalize.css', 'css/main.css', 'css/monokai_sublime.css', 'css/google-font.css'])
+        .pipe(sourcemaps.init())
         .pipe(importCss())
         .pipe(autoprefixer({
             browsers: ['last 15 versions', '> 1%', 'ie 9'],
             cascade: false
         }))
-        .pipe(concatCSS('style.css'))
-
-    // .pipe(uncss({
-    //    html: ['index.html', 'demo/form_data/index.html', 'demo/numeric_format/index.html', 'demo/inverImgText/index.html', 'demo/anchorSpy/index.html']
-    // }))
-
-    .pipe(minifyCSS())
-        .pipe(rename('style.min.css'))
+        .pipe(concat('style.min.css'))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('css/'));
 });
 
@@ -35,15 +27,11 @@ gulp.task('css', function() {
 // задача по работе с js
 gulp.task('js', function() {
     gulp.src(['js/plugins.js', 'js/main.js'])
-        .pipe(concat('all.js'))
-        .pipe(jsmin())
-        .pipe(js1k())
-        // .pipe(rename({
-        //     suffix: '.min'
-        // }))
-
-
-    .pipe(gulp.dest('js/'));
+        .pipe(sourcemaps.init())
+        .pipe(concat('all.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('js/'));
 });
 
 
